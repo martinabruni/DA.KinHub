164d985f6b3cd42f6a01df94c2767b1525751b63
Martina Bruni <martina.bruni@agic.it>
2026-07-09 17:14:52 +0200

    refactor: update using directives to use shared kernel interfaces, models, results, and enums; add audio processing enums

--- src/Domains/Kin.KinHub.KinRecipe.Domain/GlobalUsings.cs
+++ src/Domains/Kin.KinHub.KinRecipe.Domain/GlobalUsings.cs
@@ -1,5 +1,7 @@
-global using Kin.KinHub.Core.Domain.Common;
+global using Kin.KinHub.Shared.Kernel.Interfaces;
+global using Kin.KinHub.Shared.Kernel.Models;
 global using Kin.KinHub.Core.Domain.FamilyFeature;
-global using Kin.KinHub.Shared.Kernel.Common;
+global using Kin.KinHub.Shared.Kernel.Results;
+global using Kin.KinHub.Shared.Kernel.Enums;
 
 global using Kin.KinHub.Shared.Kernel.Exceptions;
